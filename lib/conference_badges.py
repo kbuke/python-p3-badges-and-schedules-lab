1c9c19e7560df7f582a99fa0ee6cbdfd89291d3d
kaan <kabuke13@gmail.com>
2024-01-22 02:50:24 +0000

    lab changes

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return(f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badge_message = []
+    for name in names:
+        badge_message.append(f"Hello, my name is {name}.")
+    return badge_message
 
 def assign_rooms(names):
-    return None
+    room_message = []
+    for i, name in enumerate(names):
+        room_message.append(f"Hello, {name}! You'll be assigned to room {i + 1}!")
+    return room_message
 
 def printer(names):
-    return None
+    badge_creator = batch_badge_creator(names)
+    room_message = assign_rooms(names)
+
+    for message in badge_creator:
+        print(message)
+
+    for message in room_message:
+        print(message)
